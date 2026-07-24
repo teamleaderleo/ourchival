7be19cf7d48853bf1a465db586dacb9f3bcbd73d
Leo <leoli_082000@hotmail.com>
2026-07-25 04:26:39 +0800

    Mount capture and conversation tools in one workbench

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,13 +1,11 @@
-import { CaptureSessionPanel } from "./CaptureSessionPanel";
-import { ConversationPanel } from "./ConversationPanel";
 import { ReferenceVault } from "./ReferenceVault";
 import { VaultAccessGate } from "./VaultAccessGate";
+import { WorkbenchDock } from "./WorkbenchDock";
 
 export default function HomePage() {
   return (
     <VaultAccessGate>
-      <CaptureSessionPanel />
-      <ConversationPanel />
+      <WorkbenchDock />
       <ReferenceVault />
     </VaultAccessGate>
   );
