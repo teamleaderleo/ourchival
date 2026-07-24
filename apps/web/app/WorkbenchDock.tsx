c9cbabe78cc46bb1771144815278e9da55243731
Leo <leoli_082000@hotmail.com>
2026-07-25 04:43:32 +0800

    Mount workbench dialog controller

--- apps/web/app/WorkbenchDock.tsx
+++ apps/web/app/WorkbenchDock.tsx
@@ -1,9 +1,11 @@
 import { CaptureSessionPanel } from "./CaptureSessionPanel";
 import { ConversationPanel } from "./ConversationPanel";
+import { WorkbenchController } from "./WorkbenchController";
 
 export function WorkbenchDock() {
   return (
     <div className="workbench-dock" aria-label="Archive workbench">
+      <WorkbenchController />
       <span className="workbench-dock-label" aria-hidden="true">
         Workbench
       </span>
